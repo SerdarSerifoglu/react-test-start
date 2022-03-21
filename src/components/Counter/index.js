4eb5d0635623d0733e8f544c22d7d8e7804c1e6f
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-03-21 21:18:57 +0300

    counter component fixed and used

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -5,8 +5,9 @@ const Counter = () => {
 
 	return (
 		<div>
-			<button onClick={() => setCount(count + 1)}>Decrease</button>
-			<button onClick={() => setCount(count - 1)}>Increase</button>
+			<p>{count}</p>
+			<button onClick={() => setCount(count - 1)}>Decrease</button>
+			<button onClick={() => setCount(count + 1)}>Increase</button>
 		</div>
 	);
 };
