3d39bc85ae7d57b3b5276079a35363e8d6f90b8f
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-03-21 22:44:38 +0300

    key press Enter

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -15,18 +15,24 @@ const defaultItems = [
 const Todo = () => {
 	const [text, setText] = useState("");
 	const [items, setItems] = useState(defaultItems);
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+			buttonClick();
+		}
+	};
 
+	const buttonClick = () => {
+		setItems((prev) => [...prev, { name: text }]);
+		setText("");
+	};
 	return (
 		<div>
-			<input value={text} onChange={(e) => setText(e.target.value)}></input>
-			<button
-				onClick={() => {
-					setItems((prev) => [...prev, { name: text }]);
-					setText("");
-				}}
-			>
-				Add
-			</button>
+			<input
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+				onKeyPress={handleKeyPress}
+			></input>
+			<button onClick={buttonClick}>Add</button>
 			<br></br>
 			{items.map((item, key) => (
 				<div key={key}>{item.name}</div>
